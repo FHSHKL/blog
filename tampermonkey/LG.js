f48db1e544c82d6fbd1b8c126aaf9d2a55e4e7b4
火骷髅 <FireHumansSkeleton@gmail.com>
2019-07-30 13:20:07 +0800

    updata LG-script

--- tampermonkey/LG.js
+++ tampermonkey/LG.js
@@ -7,24 +7,6 @@
 // @grant        none
 // ==/UserScript==
 
-function LG_search()
-{
-    console.log("LG-search");
-    var but=document.createElement("button");
-    but.setAttribute("name","searchme");
-    but.setAttribute("class","am-btn am-btn-danger");
-    but.innerHTML="ME!";
-    but.setAttribute("onclick","search_me()");
-
-    var bro=document.getElementsByName("recordsearch")[0];
-    bro.parentNode.appendChild(but);
-
-
-    var log=document.createElement("script");
-    log.innerHTML="function search_me(){var uid=document.getElementsByTagName('html')[0].innerHTML.match(/uid=[0-9]+/)[0].replace(/uid=/i,'');var ui=document.getElementsByName(\"uid\")[0];ui.nodeValue=uid;ui.value=uid;var submit=document.getElementsByName(\"recordsearch\")[0];submit.click();}";
-    document.body.appendChild(log);
-}
-
 function LG_record()
 {
     console.log("LG-record");
@@ -32,7 +14,7 @@ function LG_record()
     var ybt=document.getElementsByClassName("am-btn am-btn-sm am-btn-primary");
 
     var but=document.createElement("a");
-    but.setAttribute("href","https://www.luogu.org/recordnew/lists?uid="+uid);
+    but.setAttribute("href","https://www.luogu.org/record/list?user="+uid);
     but.setAttribute("class","am-btn am-btn-sm am-btn-primary");
     but.setAttribute("target","_blank");
     but.innerHTML="TA的提交";
@@ -40,23 +22,17 @@ function LG_record()
     ybt[0].parentNode.appendChild(but);
 }
 
-function get_new(nurl)
-{
-    if(nurl.search("problemnew/lists")>-1&&nurl.search("fe/problem/list")==-1)location.href=location.href.replace("problemnew/lists","fe/problem/list").replace("name","keyword");
-    if(nurl.search("problemnew/show")>-1&&nurl.search("fe/problem")==-1)location.href=location.href.replace("problemnew/show","fe/problem");
-    if(nurl.search("recordnew/show")>-1&&nurl.search("fe/record")==-1)location.href=location.href.replace("recordnew/show","fe/record");
-}
-
 window.onload=function()
 {
     var nurl=location.host+location.pathname;
     console.log(nurl);
-    if(nurl=="www.luogu.org/recordnew/lists")LG_search();
     if(nurl=="www.luogu.org/space/show")LG_record();
-    get_new(nurl);
 }
 
 /*
 recordnew/show
 fe/record
+
+get new lg
+
 */
