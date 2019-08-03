d230602eaed13284555950f69beef1c9a3b33f86
火骷髅 <FireHumansSkeleton@gmail.com>
2019-08-03 08:31:41 +0800

    updata LG_script_2.0.0

--- tampermonkey/LG.js
+++ tampermonkey/LG.js
@@ -4,10 +4,8 @@
  * @Website: https://huokulou.tk
  * @Description: LG_scripts
  * @Date: 2019-07-22 08:26:30
- * @LastEditTime: 2019-07-31 09:21:05
+ * @LastEditTime: 2019-08-03 08:31:25
  */
-
-const version=1;
 
 function LG_script_search_by_name()
 {
@@ -40,7 +38,15 @@ function LG_script_search_by_name()
     function LG_search_prob_slove()
     {
         var uprobname=document.getElementById("probnamesearchbox").value;
-        window.open("https://www.luogu.org/problem/list?keyword="+encodeURIComponent(uprobname));
+        var pid=uprobname.match(/(poj|cf|sp|u|t|p)[0-9]\d*/ig);
+        if(pid==null)
+        {
+            window.open("https://www.luogu.org/problem/list?keyword="+encodeURIComponent(uprobname));
+        }
+        else
+        {
+            window.open("https://www.luogu.org/problem/"+pid[0]);
+        }
     }
     document.getElementById("probnamesearch").onclick=function(){LG_search_prob_slove();};
     $(document.getElementById("probnamesearchbox")).keydown(function (e) {if (e.keyCode==13){LG_search_prob_slove();}});
@@ -63,17 +69,6 @@ function LG_script_record()
     ybt[0].parentNode.appendChild(but);
 }
 
-function LG_script_login()
-{
-    console.log("LG-login");
-    var s=document.getElementsByClassName("frame lfe-form-sz-middle");
-    for(var i=0;i<s.length;i++)
-    {
-        var now=s[i];
-        now.onkeypress=function doit(e){if(e.keyCode==13){document.getElementsByClassName("btn-login lfe-form-sz-middle")[0].click();}};
-    }
-}
-
 function LG_script_init()
 {
     var nurl=location.host+location.pathname;
@@ -81,13 +76,4 @@ function LG_script_init()
     console.log("当前地址:"+nurl);
     if(nurl=="www.luogu.org/space/show")LG_script_record();
     if(nurl=="www.luogu.org/")LG_script_search_by_name();
-    if(nurl=="www.luogu.org/auth/login")LG_script_login();
 }
-
-/*
-recordnew/show
-fe/record
-
-get new lg
-
-*/
